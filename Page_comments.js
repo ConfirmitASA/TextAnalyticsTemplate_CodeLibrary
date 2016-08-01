84e8842f980fc63f2b996aae597009fedcd60645
Son-tsss <marya-sokolova@yandex.ru>
2016-08-01 13:27:15 +0300

    C: hitlist

---
+++ Page_comments.js
@@ -0,0 +1,54 @@
+class Page_detailed_analysis {
+    static private const PageId = "detailed_analysis";
+
+    static function Hide(context) {
+        return false
+    }
+    static function Render(context) {
+        var metaData = new MetaData(context.report, context.log);
+        var parameterUtilities = new ParameterUtilities(context.report, context.state, context.log);
+        var pageTemplate = new PageTemplate(context.component, context.report, parameterUtilities, context.log);
+        pageTemplate.Process(PageId);
+
+        TALibrary.setReport(context.pageContext, context.log, context.report, context.confirmit, context.user);
+        TALibrary.setCurrentQuestion(context.pageContext.Items["questionID"]);
+        TALibrary.currentQuestion.setCurrentTheme(context.state.Parameters.IsNull("TA_TOP_CATEGORIES_SINGLE") ? null : context.state.Parameters.GetString("TA_TOP_CATEGORIES_SINGLE"));
+
+        if(context.state.Parameters.IsNull("TA_DISTRIBUTION_TOGGLE"))
+            context.state.Parameters["TA_DISTRIBUTION_TOGGLE"] = new ParameterValueResponse("0");
+
+        if(context.state.Parameters.IsNull("TA_VIEW_BY"))
+            context.state.Parameters["TA_VIEW_BY"] = new ParameterValueResponse("0");
+    }
+
+    static function txtCategory_Hide(context){
+        return false;
+    }
+    static function txtCategory_Render(context){
+        context.component.Output.Append("Category");
+    }
+
+    static function lstCategory_Hide(context){
+        return false;
+    }
+
+    static function htlComments_Hide(context){
+        return false;
+    }
+
+    static function htlComments_Render(context){
+        if(TALibrary.currentQuestion.currentTheme>=0){
+            context.component.Columns.Add(TALibrary.getTAHitlistColumn("categorysentiment"));
+        }
+        context.component.Columns.Add(TALibrary.getTAHitlistColumn("verbatim"));
+        context.component.Columns.Add(TALibrary.getTAHitlistColumn(Config.DateVariableId ? Config.DateVariableId : "interview_start"));
+        context.component.Columns.Add(TALibrary.getTAHitlistColumn("overallsentiment"));
+
+        for(var i = 0 ; i < TALibrary.currentQuestion.questionDetails.TAHitlistFields.length; i++){
+            context.component.Columns.Add(TALibrary.getTAHitlistColumn(TALibrary.currentQuestion.questionDetails.TAHitlistFields[i]));
+        }
+
+    }
+
+
+}
---
+++ TAHitlistUtils.js
@@ -0,0 +1,58 @@
+class TAHitlistUtils {
+    static var pageContext: ScriptPageContext;
+    static var log: Logger;
+    static var report: Report;
+    static var confirmit: ConfirmitFacade;
+    static var user: User;
+
+    /**
+     * @param {Logger} l - log
+     * @param {Report} r - report
+     * @param {ConfirmitFacade} c - confirmit
+     * @param {User} u - user
+     */
+    static function setGlobals(p: ScriptPageContext, l: Logger, r: Report, c: ConfirmitFacade, u: User){
+    pageContext = p;
+    log = l;
+    report = r;
+    confirmit = c;
+    user = u;
+}
+    /**
+     * function to add column to hitlist
+     * @param {String} name - question id or TA postfix
+     * @return {HitListColumn}
+     */
+    static function
+
+    getTAHitlistColumn(name){
+        var hitlistColumn;
+        HitListColumn = new HitListColumn();
+
+        switch (name.toLowerCase()) {
+            case "overallsentiment":
+                hitlistColumn.QuestionnaireElement = TALibrary.currentQuestion.overallSentiment.questionnaireElement;
+                break;
+            case "categories":
+                hitlistColumn.QuestionnaireElement = TALibrary.currentQuestion.categories.questionnaireElement;
+                break;
+            case "positivementions":
+                hitlistColumn.QuestionnaireElement = TALibrary.currentQuestion.positiveMentions.questionnaireElement;
+                break;
+            case "negativementions":
+                hitlistColumn.QuestionnaireElement = TALibrary.currentQuestion.negativeMentions.questionnaireElement;
+                break;
+            case "categorysentiment":
+                hitlistColumn.QuestionnaireElement = TALibrary.currentQuestion.currentTheme>=0 ? TALibrary.currentQuestion.project.CreateQuestionnaireElement(TALibrary.currentQuestion.categorySentiment.questionName, TALibrary.currentQuestion.themes[TALibrary.currentQuestion.currentTheme].id) : TALibrary.currentQuestion.categorySentiment.questionnaireElement;
+                break;
+            case "verbatim":
+                hitlistColumn.QuestionnaireElement = TALibrary.currentQuestion.verbatim.questionnaireElement;
+                break;
+            default:
+                hitlistColumn.QuestionnaireElement = TALibrary.currentQuestion.project.CreateQuestionnaireElement(name);
+                break;
+        }
+
+        return hitlistColumn
+    }
+}
