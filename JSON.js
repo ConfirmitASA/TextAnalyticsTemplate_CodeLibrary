2e280aceecaa07dcc53a2f5dcd5c08e0f3aa5e9b
Son-tsss <marya-sokolova@yandex.ru>
2016-08-08 13:01:59 +0300

    DA: workaround for reportal geting rowheaders bug

--- JSON.js
+++ JSON.js
@@ -90,4 +90,31 @@ class TATableData{
         }
         return blocks;
     }
+
+
+    static function createDetailedAnalysisHeader(rowheadersObject,hierarchyObject,blockId,index){
+        rowheadersObject[hierarchyObject.id+(blockId?("_"+blockId):null)]= {
+            title: hierarchyObject.name,
+            index: index,
+            categoryId: id,
+            blockId: blockId
+        }
+        index++;
+        for(var i =0; i<hierarchyObject.children.length;i++){
+            createDetailedAnalysisHeader(rowheadersObject,hierarchyObject.children[i],blockId,index);
+        }
+    }
+
+    static function getDetailedAnalysisHeaders(blocks) {
+        var rowheadersObject = {};
+        var index = 0;
+        var themesArray = [];
+        if (blocks.length > 0) blocks.push(null);
+        TALibrary.currentQuestion.currentTheme > -1 ? themesArray.push(TALibrary.currentQuestion.themes[TALibrary.currentQuestion.currentTheme]) : themesArray = TALibrary.currentQuestion.themes;
+        for (var i = 0; i < blocks.length; i++) {
+            for (var j = 0; j < themesArray.length; j++) {
+                createDetailedAnalysisHeader(rowheadersObject,themesArray[j],blocks[i],index);
+            }
+        }
+    }
 }
