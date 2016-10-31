bd8f98a35279bd6864a842b128315f310862c66b
Son-tsss <marya-sokolova@yandex.ru>
2016-10-31 15:54:48 +0300

    fixing time variable id configuration bug

--- TAFolder.js
+++ TAFolder.js
@@ -28,7 +28,7 @@ class TAFolder{
         _id = config.TAQuestions[questionIndex].TAFolderId;
         _qName = config.TAQuestions[questionIndex].TAQuestionName;
         _modelNo = config.TAQuestions[questionIndex].TAModelNo;
-
+    globals.log.LogDebug("config time var 1: "+ config.TimeVariableId);
         _timeVariableId = TAHelper.GetConfiguredVariables(globals,[config.TAQuestions[questionIndex].TimeVariableId], [config.TimeVariableId], null, ["interview_start"])[0];
         globals.log.LogDebug("config time var: "+ _timeVariableId);
 
