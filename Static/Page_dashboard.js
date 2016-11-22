d7e692278a7eb7c6e0325305dfb77f37a34fac2e
Son-tsss <marya-sokolova@yandex.ru>
2016-11-22 12:31:11 +0300

    Add folders parameter

--- Static/Page_dashboard.js
+++ Static/Page_dashboard.js
@@ -51,8 +51,13 @@ class Page_dashboard{
         var paramUtils = new ParameterUtilities(TAHelper.GetGlobals(context));
         paramUtils.SetDefaultParameterValues(_defaultParameters);
         var taParams  = new TAParameters(TAHelper.GetGlobals(context), Config.GetTALibrary());
-        var selectedFolder = !context.state.Parameters.IsNull("TA_FOLDERS") ? context.state.Parameters.GetString("TA_FOLDERS") : null;
-        _folder =Config.GetTALibrary().GetFolderById(selectedFolder);
+        var selectedFolder;
+        try {
+            selectedFolder = !context.state.Parameters.IsNull("TA_FOLDERS1") ? context.state.Parameters.GetString("TA_FOLDERS") : null;
+        }catch(e){
+            selectedFolder = null;
+        }
+            _folder = Config.GetTALibrary().GetFolderById(selectedFolder);
         taParams.ClearSubcategoriesParameters(null, "emptyv", "TA_TOP_CATEGORIES_SINGLE", "TA_SUB_CATEGORIES_SINGLE", "TA_ATTRIBUTES_SINGLE");
         taParams.ClearSubcategoriesParameters(null, "emptyv", "TA_SUB_CATEGORIES_SINGLE", "TA_ATTRIBUTES_SINGLE");
     }
