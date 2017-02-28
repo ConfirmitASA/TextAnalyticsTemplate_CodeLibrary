877b9fd804abd343dee805e5af989ceff23493e9
Son-tsss <marya-sokolova@yandex.ru>
2017-02-28 16:29:26 +0300

    Translations

--- Static/PermissionScript.js
+++ Static/PermissionScript.js
@@ -2,7 +2,7 @@ class PermissionScript{
     static function Render(context){
     if (!context.state.Parameters.GetString('LANGUAGE'))
         context.report.CurrentLanguage = 9;
-        else if(context.state.Parameters.GetString('LANGUAGE') != parseInt(context.report.CurrentLanguage))
+        else if(parseInt(context.state.Parameters.GetString('LANGUAGE')) != context.report.CurrentLanguage)
             context.report.CurrentLanguage = parseInt(context.state.Parameters.GetString('LANGUAGE'));
 }
 }
