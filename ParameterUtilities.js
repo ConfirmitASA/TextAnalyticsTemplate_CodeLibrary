1dff742bf8ffd9b4af7a2dfb6c0f18e617c7b931
son-tsss <marya-sokolova@yandex.ru>
2017-06-20 16:44:34 +0300

    pages refactoring

--- ParameterUtilities.js
+++ ParameterUtilities.js
@@ -85,7 +85,7 @@ class ParameterUtilities {
     context.log.LogDebug("defa2");
         var parameterValues = params.parameterValues;
     context.log.LogDebug("defa3");
-        for (var i = 0; i < valuesArray.length; i++) {
+        for (var i = 0; i < parameterValues.length; i++) {
             try {
                 if (!context.state.Parameters.GetString(valuesArray[i].Id))
                     context.state.Parameters[parameterValues[i].Id] = new ParameterValueResponse(parameterValues[i].Value);
