54651abafb7242bca72bed26fa85f767b5f4ea42
annyrtd <fyz95@mail.ru>
2017-11-03 12:55:33 +0300

    Update comment

--- TAParameterUtilities.js
+++ TAParameterUtilities.js
@@ -83,7 +83,7 @@ class TAParameterUtilities {
 
     /**
      * @memberof TAParameterUtilities
-     * @function SetDefaultParameterValues
+     * @function SetDefaultParameterValuesForEmpty
      * @description function to set default value to parameters only if they haven't been selected
      * @param {Object} params - {
      *          context: {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log},
