c895fa2d086defd2bab9592cbc2b93ae5c21e738
Anna Kozlova <fyz95@mail.ru>
2018-10-10 16:33:46 +0300

    Page_customer_journey refactoring

--- Static/Page_customer_journey.js
+++ Static/Page_customer_journey.js
@@ -5,17 +5,6 @@
 class Page_customer_journey{
     /**
      * @memberof Page_customer_journey
-     * @function Hide
-     * @description function to hide the page
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
-     */
-    static function Hide(context){
-        return false;
-    }
-
-    /**
-     * @memberof Page_customer_journey
      * @function Render
      * @description function to render the page
      * @param {Object} context - {component: page, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
@@ -32,13 +21,49 @@ class Page_customer_journey{
 
     /**
      * @memberof Page_customer_journey
-     * @function tblCustomerJourneyTrend_Hide
-     * @description function to render table for Customer Journey Trend Chart
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
+     * @function tblCustomerJourneyCards_Render
+     * @description function to render table for Customer Journey cards
+     * @param {Object} context - {component: table, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
      */
-    static function tblCustomerJourneyTrend_Hide(context){
-        return false
+    static function tblCustomerJourneyCards_Render(context){
+        var selectedFolder = TALibrary.GetTAFoldersParameterValue(context);
+        var folder = Config.GetTALibrary().GetFolderById(selectedFolder);
+        var cardsTable = new TACustomerJourneyCardsTable({folder:folder, config:Config, context:context});
+        cardsTable.GetTATableUtils().ClearTableDistributions();
+    }
+
+    /**
+     * @memberof Page_customer_journey
+     * @function btnCustomerJourneyCards_Render
+     * @description function to render button that leads to the Correlation page after clicking on a CJ card
+     * @param {Object} context - {component: button, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+     */
+    static function btnCustomerJourneyCards_Render(context){
+        context.component.TargetPage = 'correlation';
+    }
+
+    /**
+     * @memberof Page_customer_journey
+     * @function txtCustomerJourneyCardsScript_Render
+     * @description function to print Customer Journey cards script
+     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+     */
+    static function txtCustomerJourneyCardsScript_Render(context){
+        var cardsInit = "<script>" +
+            "new Reportal.CustomerJourneyCards({" +
+            "translations: translations," +
+            "tableContainerId:'cj_drilldown'," +
+            "drilldownId:'cj_drilldown'," +
+            "cardContainerId: 'cj_cards'," +
+            "CJ_options: CJ_options});" +
+            "</script>";
+
+        var currentLanguage = context.report.CurrentLanguage;
+        var currentDictionary = Translations.dictionary(currentLanguage);
+        context.component.Output.Append(JSON.print(currentDictionary,"translations"));
+
+        context.component.Output.Append(JSON.print(context.pageContext.Items['options'], 'CJ_options'));
+        context.component.Output.Append(cardsInit);
     }
 
     /**
@@ -58,13 +83,52 @@ class Page_customer_journey{
 
     /**
      * @memberof Page_customer_journey
-     * @function txtCustomerJourneyTrendScript_Hide
-     * @description function to render Customer Journey Trend Chart
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
+     * @function btnDetailedAnalysisDrilldown_Render
+     * @description function to render button that leads to the Detailed Analysis page after clicking on a CJ card
+     * @param {Object} context - {component: button, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
      */
-    static function txtCustomerJourneyTrendScript_Hide(context){
-        return false
+    static function btnDetailedAnalysisDrilldown_Render(context){
+        context.component.TargetPage = 'detailed_analysis';
+    }
+
+    /**
+     * @memberof Page_customer_journey
+     * @function txtCustomerJourneyTrendTitle_Render
+     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+     */
+    static function txtCustomerJourneyTrendTitle_Render(context){
+        var currentLanguage = context.report.CurrentLanguage;
+        var currentDictionary = Translations.dictionary(currentLanguage);
+        var label = currentDictionary["Trend chart"];
+        context.component.Output.Append(label);
+    }
+
+    /**
+     * @memberof Page_customer_journey
+     * @function txtComparePeriods_Render
+     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+     */
+    static function txtComparePeriods_Render(context){
+        var currentLanguage = context.report.CurrentLanguage;
+        var currentDictionary = Translations.dictionary(currentLanguage);
+        var label = currentDictionary["Compare"];
+        context.component.Output.Append(label);
+
+        context.component.Output.Append(TAParameterValues.getParameterValue(context.state, currentDictionary, 'TA_PERIOD'));
+    }
+
+    /**
+     * @memberof Page_customer_journey
+     * @function txtViewBy_Render
+     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+     */
+    static function txtViewBy_Render(context){
+        var currentLanguage = context.report.CurrentLanguage;
+        var currentDictionary = Translations.dictionary(currentLanguage);
+        var label = currentDictionary['View by:'];
+        context.component.Output.Append(label);
+
+        context.component.Output.Append(TAParameterValues.getParameterValue(context.state, currentDictionary, 'TA_TREND_LINE_VIEW_BY'));
     }
 
     /**
@@ -100,157 +164,5 @@ class Page_customer_journey{
         context.component.Output.Append(JSON.print(currentDictionary,"translations"));
         context.component.Output.Append(JSON.print(palette,"palette"));
         context.component.Output.Append(chartInit);
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function tblCustomerJourneyCards_Hide
-     * @description function to hide table for Customer Journey cards
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
-     */
-    static function tblCustomerJourneyCards_Hide(context){
-        return false
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function tblCustomerJourneyCards_Render
-     * @description function to render table for Customer Journey cards
-     * @param {Object} context - {component: table, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     */
-    static function tblCustomerJourneyCards_Render(context){
-        var selectedFolder = TALibrary.GetTAFoldersParameterValue(context);
-        var folder = Config.GetTALibrary().GetFolderById(selectedFolder);
-        var cardsTable = new TACustomerJourneyCardsTable({folder:folder, config:Config, context:context});
-        cardsTable.GetTATableUtils().ClearTableDistributions();
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function lstCustomerJourneyCards_Hide
-     * @description function to hide list with all CJ items
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
-     */
-    static function lstCustomerJourneyCards_Hide(context){
-        return false
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function btnCustomerJourneyCards_Hide
-     * @description function to hide button that leads to the Correlation page after clicking on a CJ card
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
-     */
-    static function btnCustomerJourneyCards_Hide(context){
-        return false
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function btnCustomerJourneyCards_Render
-     * @description function to render button that leads to the Correlation page after clicking on a CJ card
-     * @param {Object} context - {component: button, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     */
-    static function btnCustomerJourneyCards_Render(context){
-        context.component.TargetPage = 'correlation';
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function txtCustomerJourneyCardsScript_Hide
-     * @description function to hide Customer Journey cards script
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
-     */
-    static function txtCustomerJourneyCardsScript_Hide(context){
-        return false
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function txtCustomerJourneyCardsScript_Render
-     * @description function to print Customer Journey cards script
-     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     */
-    static function txtCustomerJourneyCardsScript_Render(context){
-        var cardsInit = "<script>" +
-            "new Reportal.CustomerJourneyCards({" +
-            "translations: translations," +
-            "tableContainerId:'cj_drilldown'," +
-            "drilldownId:'cj_drilldown'," +
-            "cardContainerId: 'cj_cards'," +
-            "CJ_options: CJ_options});" +
-            "</script>";
-
-        var currentLanguage = context.report.CurrentLanguage;
-        var currentDictionary = Translations.dictionary(currentLanguage);
-        context.component.Output.Append(JSON.print(currentDictionary,"translations"));
-
-        context.component.Output.Append(JSON.print(context.pageContext.Items['options'], 'CJ_options'));
-        context.component.Output.Append(cardsInit);
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function txtCustomerJourneyTrendTitle_Hide
-     * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     * @returns {Boolean}
-     */
-    static function txtCustomerJourneyTrendTitle_Hide(context){
-        return false;
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function txtCustomerJourneyTrendTitle_Render
-     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     */
-    static function txtCustomerJourneyTrendTitle_Render(context){
-        var currentLanguage = context.report.CurrentLanguage;
-        var currentDictionary = Translations.dictionary(currentLanguage);
-        var label = currentDictionary["Trend chart"];
-        context.component.Output.Append(label);
-    }
-
-
-    /**
-     * @memberof Page_customer_journey
-     * @function btnDetailedAnalysisDrilldown_Render
-     * @description function to render button that leads to the Detailed Analysis page after clicking on a CJ card
-     * @param {Object} context - {component: button, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     */
-    static function btnDetailedAnalysisDrilldown_Render(context){
-        context.component.TargetPage = 'detailed_analysis';
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function txtComparePeriods_Render
-     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     */
-    static function txtComparePeriods_Render(context){
-        var currentLanguage = context.report.CurrentLanguage;
-        var currentDictionary = Translations.dictionary(currentLanguage);
-        var label = currentDictionary["Compare"];
-        context.component.Output.Append(label);
-
-        context.component.Output.Append(TAParameterValues.getParameterValue(context.state, currentDictionary, 'TA_PERIOD'));
-    }
-
-    /**
-     * @memberof Page_customer_journey
-     * @function txtViewBy_Render
-     * @param {Object} context - {component: text, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
-     */
-    static function txtViewBy_Render(context){
-        var currentLanguage = context.report.CurrentLanguage;
-        var currentDictionary = Translations.dictionary(currentLanguage);
-        var label = currentDictionary['View by:'];
-        context.component.Output.Append(label);
-
-        context.component.Output.Append(TAParameterValues.getParameterValue(context.state, currentDictionary, 'TA_TREND_LINE_VIEW_BY'));
     }
 }
