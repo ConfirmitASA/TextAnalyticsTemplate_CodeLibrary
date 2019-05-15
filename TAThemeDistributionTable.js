dddd5be4b988e6e7f9374246f1c7d890bc9e0334
Maria Senina <maria.senina@confirmit.com>
2019-05-15 17:46:57 +0300

    % top2box

--- TAThemeDistributionTable.js
+++ TAThemeDistributionTable.js
@@ -125,7 +125,7 @@ class TAThemeDistributionTable{
      * @memberof TAThemeDistributionTable
      * @private
      * @instance
-     * @description to calculate count, avg.sentiment and stdev we use statistics header, data is filtered by scale mast in rowheaders
+     * @description to calculate count, %top boxes, values% and stdev we use statistics header, data is filtered by scale mast in rowheaders
      * @function _getHeaderStatistics
      * @returns {Header}
      */
@@ -137,35 +137,37 @@ class TAThemeDistributionTable{
         countHeader.Decimals = 0;
         countHeader.HideHeader = true;
         columnsCollection.Add(countHeader);
-
-        var avgHeader : HeaderStatistics = new HeaderStatistics();
-        avgHeader.Statistics.Avg = true;
-        avgHeader.Decimals = 1;
-        avgHeader.HideHeader = true;
-        columnsCollection.Add(avgHeader);
-
+        /*
+                var avgHeader : HeaderStatistics = new HeaderStatistics();
+                avgHeader.Statistics.Avg = true;
+                avgHeader.Decimals = 1;
+                avgHeader.HideHeader = true;
+                columnsCollection.Add(avgHeader);
+        */
         var categoriesHeader : HeaderCategories =  new HeaderCategories();
         var positiveCodesMask : GenericCodeMask = new GenericCodeMask();
         positiveCodesMask.Type = MaskType.ShowCodes;
         positiveCodesMask.Codes = Config.SentimentRange.Positive.join(",");
-        //positiveCodesMask.Codes = "8,9,10";
         categoriesHeader.Mask = positiveCodesMask;
+        categoriesHeader.Totals = false;
+        categoriesHeader.HideData = true;
         columnsCollection.Add(categoriesHeader);
 
         var topBoxFormulaHeader : HeaderFormula = new HeaderFormula();
         topBoxFormulaHeader.Type = FormulaType.Expression;
         var cellsExpression = "";
         for(var i = 0; i < Config.SentimentRange.Positive.length; i++)
-            cellsExpression += "cellv(col - " + i + ", row) +"
+            cellsExpression += "if (cellv(col - " + (i + 1) + ", row) > 0, cellv(col - " + (i + 1) + ", row), 0) + "
         cellsExpression += "0";
-        topBoxFormulaHeader.Expression = "if(cellv(col- " + Config.SentimentRange.Positive.length + 2 + ", row) > 0, ( " + cellsExpression + ")/cellv(col- " + Config.SentimentRange.Positive.length + 2 + ", row), emptyv())";
+        topBoxFormulaHeader.Expression = "if(cellv(col- " + (Config.SentimentRange.Positive.length + 1) + ", row) > 0," +
+            "( " + cellsExpression + ")/cellv(col- " + (Config.SentimentRange.Positive.length + 1) + ", row), cellv(col- " + (Config.SentimentRange.Positive.length + 1) + ", row))";
         topBoxFormulaHeader.Percent = true;
         topBoxFormulaHeader.Decimals = 0;
         columnsCollection.Add(topBoxFormulaHeader);
 
         var percentVolumeHeader : HeaderFormula = new HeaderFormula();
         percentVolumeHeader.Type = FormulaType.Expression;
-        percentVolumeHeader.Expression = "cellv(col-2, row)/cellv(col-2, 1)";
+        percentVolumeHeader.Expression = "cellv(col- " + (Config.SentimentRange.Positive.length + 2) +", row)/cellv(col-" + (Config.SentimentRange.Positive.length + 2) +", 1)";
         percentVolumeHeader.Percent = true;
         percentVolumeHeader.Decimals = 1;
         columnsCollection.Add(percentVolumeHeader);
