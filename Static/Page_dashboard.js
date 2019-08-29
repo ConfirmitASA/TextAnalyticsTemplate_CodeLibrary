9b05a28ce0a143fdebc7d0db8e4ce14da7ea5478
Maria Senina <maria.senina@confirmit.com>
2019-08-29 13:24:32 +0300

    hide top5 section in print mode

--- Static/Page_dashboard.js
+++ Static/Page_dashboard.js
@@ -418,6 +418,7 @@ class Page_dashboard{
                 "var top5SectionElements = document.querySelectorAll('.top5');" +
                 "for (var i = 0; i < top5SectionElements.length; i++) {" +
                 "top5SectionElements[i].classList += ' top5_hidden'" +
+                "top5SectionElements[i].style.display = 'none'" +
                 "}" +
                 "</script>";
             context.component.Output.Append(hideTop5Script);
