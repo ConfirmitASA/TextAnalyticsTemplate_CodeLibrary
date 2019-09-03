1cf5d7fec23e0b9231e9b1c0c186c9cec4a4d9c4
Maria Senina <maria.senina@confirmit.com>
2019-09-03 17:27:07 +0300

    pdf export IA page

--- Static/Page_correlation.js
+++ Static/Page_correlation.js
@@ -161,7 +161,7 @@ class Page_correlation{
             "   chartContainer: 'correlation-chart'," +
             "   tableContainer: 'correlation-tables-view'," +
             "   buttonsContainer: 'chart-tables-switcher'," +
-            "   table: document.querySelector('.dashboard__widget .correlation-table')," +
+            "   table: document.querySelector('article .correlation-table')," +
             "   palette: palette," +
             "   questionName: '" + (correlationVariableName || correlationQuestion.Title || correlationVariableId) + "'," +
             "   translations: translations" +
